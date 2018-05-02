880ec2f507ffa2835229b3f7282b4bf0ce87ca17
Mark Johnson <markjohnson4542@gmail.com>
2018-05-02 15:15:31 -0400

    added example for 'use strict'

--- strict.js
+++ strict.js
@@ -1,7 +1,18 @@
 // Strict mode -- makes debugging easier
-// code areas that would otherwise have been ignored
+// code errors that would otherwise have been ignored
 
  // *** "use strict" // <--- how to enable it! -- must be string
+
+/* uses
+
+ 1)Let's you know when you've forgotten to declare a variable
+ 2) Stops you from using words that are reserved for future versions of JavaScript
+ 3) Can't delete functions, variables, or function arguments
+ 4) Makes eval keyword a little safer
+*/
+
+
+
 
 // why a string? when it was first implemented all browsers didn't support
 // so if not a string an older browser would have failed, but if it's a string an older browser would just be like hey it's a string no biggie
@@ -19,7 +30,7 @@ function newCode() {
 }
 
 /////////
-// Example
+// Example 1
 ////////
 
 /* Without strict mode, assigning a value to an undeclared variable will automatically create a
@@ -38,3 +49,26 @@ if (theVal > 0) {
 
 // you would expect to see hello since you assigned theVal to 1, however you made a syntax error.
 // if using "use strict" at the top of the file you will get an error saying thVal is not defined!!!
+
+
+
+////////////////////
+//// Example 2
+///////////////////
+
+
+
+////////////////////
+//// Example 3
+///////////////////
+
+
+////////////////////
+//// Example 4
+///////////////////
+
+var a = 2
+eval("var a = 1")
+console.log(a)
+
+/*
